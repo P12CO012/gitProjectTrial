8702df805cb1c1460428418e84e6c1378a01c913
P12CO012 <115061743+P12CO012@users.noreply.github.com>
2023-12-04 18:15:49 +0530

    Update greetings.java

--- src/gitProject/greetings.java
+++ src/gitProject/greetings.java
@@ -5,7 +5,7 @@ public class greetings {
 	public static void main(String[] args) 
 	{
 		System.out.println("our code is here");
-		System.out.println("changes done locally on Branch_1 ");
+		System.out.println("changes done locally on Branch_1 and changed on repository");
 	}
 
 }
