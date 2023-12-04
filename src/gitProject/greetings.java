4e7c2d4fbfb65a3a0e31ca392d0010a01f6445d7
P12CO012 <115061743+P12CO012@users.noreply.github.com>
2023-12-04 16:47:09 +0530

    Update greetings.java

--- src/gitProject/greetings.java
+++ src/gitProject/greetings.java
@@ -4,7 +4,7 @@ public class greetings {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("!!WARNING CONFLICT HERE!!!!");
+		System.out.println("!!code by some other developer!!!!");
 		System.out.println("changes done before pull ");
 	}
 
