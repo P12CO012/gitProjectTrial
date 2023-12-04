64b6c38b4d3b2c769627ac40a8716d7cf3a99e80
Shubham <41320595+AlphaRaxtor@users.noreply.github.com>
2023-12-04 16:14:40 +0530

    Update greetings.java
    conflit demo

--- src/gitProject/greetings.java
+++ src/gitProject/greetings.java
@@ -4,7 +4,7 @@ public class greetings {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("hi there...creating conflict here ");
+		System.out.println("!!WARNGIN CONFLIC!!!!");
 		System.out.println("changes done before pull ");
 	}
 
