980843385581c5d27abcd15c344b23410825b4c6
P12CO012 <115061743+P12CO012@users.noreply.github.com>
2023-12-04 16:00:32 +0530

    Update greetings.java
    created conflict at line no 7

--- src/gitProject/greetings.java
+++ src/gitProject/greetings.java
@@ -4,7 +4,7 @@ public class greetings {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("hi there...creating conflict ");
+		System.out.println("hi there...creating conflict here ");
 		System.out.println("changes done before pull ");
 	}
 
